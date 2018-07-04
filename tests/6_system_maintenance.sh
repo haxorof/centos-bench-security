bc71e46280fe936f3564869d543980985eb3be96
Phil Massyn <massyn@users.noreply.github.com>
2018-07-05 06:17:41 +0900

    Adding 6.1.1

--- tests/6_system_maintenance.sh
+++ tests/6_system_maintenance.sh
@@ -3,7 +3,7 @@
 check_6() {
   info "6 - System Maintenance"
   info "6.1    - System File Permissions"
-  todo "6.1.1  - Audit system file permissions (Not Scored)"
+  test_wrapper 0 "6.1.1  - Audit system file permissions (Not Scored)" test_system_file_perms
   test_wrapper 0 "6.1.2  - Ensure permissions on /etc/passwd are configured (Scored)" test_permissions_0644_root_root ${PASSWD}
   test_wrapper 0 "6.1.3  - Ensure permissions on /etc/shadow are configured (Scored)" test_permissions_0000_root_root ${SHADOW}
   test_wrapper 0 "6.1.4  - Ensure permissions on /etc/group are configured (Scored)" test_permissions_0644_root_root ${GROUP}
